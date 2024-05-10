5b0707a59d2645459ca81bcc912aa217d6cac67a
Chris Brooksbank <brooksbankc@parliament.uk>
2024-05-10 11:24:59 +0100

    added secret connectionstring for dev

--- admin/Program.cs
+++ admin/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Configure settings
 var configurationBuilder = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddUserSecrets<Program>();
 var configuration = configurationBuilder.Build();
 builder.Services.Configure<AzureStorageSettings>(configuration.GetSection("AzureStorage"));
 
